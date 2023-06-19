49ae2d6c128c97a46b52821bec5e1ee9417b251b
Madhumita Subramaniam <madhu@gluu.org>
2023-06-19 14:39:29 +0530

    feat: add Credentials.java

---
+++ projects/passwurd/lib/passwurd/Credentials.java
@@ -0,0 +1,27 @@
+package passwurd;
+
+public class Credentials {
+
+	private String username;
+	private String k_username;
+	private String k_pwd;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getK_username() {
+		return k_username;
+	}
+	public void setK_username(String k_username) {
+		this.k_username = k_username;
+	}
+	public String getK_pwd() {
+		return k_pwd;
+	}
+	public void setK_pwd(String k_pwd) {
+		this.k_pwd = k_pwd;
+	}
+
+}
